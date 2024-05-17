755343884b0a8e724308a8f2ea6564c8b1750226
Peter <peterkanyi19193@gmail.com>
2024-05-17 07:11:42 +0300

    adds state and events lab

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,19 +1,25 @@
-import React from "react";
-import Item from "./Item";
+import React, { useState } from "react";
+import Item from "./Item"
 
 function ShoppingList({ items }) {
+  const [selectedCategory, setSelectedCategory] = useState("All")
+  const handleCategoryChange = (event) => {
+    setSelectedCategory(event.target.value);
+  };
+  const filteredItems = items.filter((item) => selectedCategory ===  "All" || item.category === selectedCategory
+)
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" value={selectedCategory} onChange={handleCategoryChange}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
-          <option value="Dessert">Dessert</option>
+          <option value="Dessert">Desert</option>
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {filteredItems.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
